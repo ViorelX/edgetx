087c07bae8a2d3333d29c77e45ea3b3edd59ef8e
Nascov Victor <83503278+ViorelX@users.noreply.github.com>
2021-07-03 12:53:56 +0300

    Update hal.h

--- radio/src/targets/horus/hal.h
+++ radio/src/targets/horus/hal.h
@@ -1017,22 +1017,23 @@
 #endif
 
 #if defined(RADIO_T18) && defined(INTERNAL_GPS)
-#define GPS_RCC_AHB1Periph            RCC_AHB1Periph_GPIOE
-#define GPS_RCC_APB1Periph            RCC_APB1Periph_UART8
+#define GPS_RCC_AHB1Periph            RCC_AHB1Periph_GPIOC
+#define GPS_RCC_APB1Periph            RCC_APB1Periph_UART4
 #define GPS_RCC_APB2Periph            0
-#define GPS_USART                     UART8
-#define GPS_GPIO_AF                   GPIO_AF_UART8
-#define GPS_USART_IRQn                UART8_IRQn
-#define GPS_USART_IRQHandler          UART8_IRQHandler
-#define GPS_UART_GPIO                 GPIOE
-#define GPS_TX_GPIO_PIN               GPIO_Pin_1 // PE.01
-#define GPS_RX_GPIO_PIN               GPIO_Pin_0 // PE.00
-#define GPS_TX_GPIO_PinSource         GPIO_PinSource1
-#define GPS_RX_GPIO_PinSource         GPIO_PinSource0
+#define GPS_USART                     UART4
+#define GPS_GPIO_AF                   GPIO_AF_UART4
+#define GPS_USART_IRQn                UART4_IRQn
+#define GPS_USART_IRQHandler          UART4_IRQHandler
+#define GPS_UART_GPIO                 GPIOC
+#define GPS_TX_GPIO_PIN               GPIO_Pin_10 // PC.10
+#define GPS_RX_GPIO_PIN               GPIO_Pin_11 // PC.11
+#define GPS_TX_GPIO_PinSource         GPIO_PinSource10
+#define GPS_RX_GPIO_PinSource         GPIO_PinSource11
 #elif !defined(INTERNAL_GPS)
   #define GPS_RCC_AHB1Periph            0
   #define GPS_RCC_APB1Periph            0
   #define GPS_RCC_APB2Periph            0
 #endif
+
 
 #endif // _HAL_H_
